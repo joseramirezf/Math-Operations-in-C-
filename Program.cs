ae74717da6dbe636d24afd8b71ae6ade6fdb05a8
Jose Ramirez <joseramirez@Joses-MacBook-Pro.local>
2022-06-08 13:36:47 -0600

    My first math operations

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Suma
+Console.WriteLine(3 + 2);
+
+// Resta
+Console.WriteLine(45 - 20);
+
+// Multiplicación
+Console.WriteLine(35 * 25);
+
+// Division
+Console.WriteLine(Decimal.Divide(51, 3)); 
+
+// Exponente
+Console.WriteLine(Math.Pow(6, 3));
+
+// Division entera
+Console.WriteLine(32 / 2);
+
+// Módulo
+Console.WriteLine(45 % 4);
